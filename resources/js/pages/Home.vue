b71e8807ef0ea1ea59d508e9366934cb071dcf6b
Benoît Ripoche <ripoche.b@gmail.com>
2018-09-13 17:50:30 +0200

    pirate ipsum

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -5,7 +5,10 @@
 
             <div class="card-body">
                 <p>
-                    Incenderat autem audaces usque ad insaniam homines ad haec, quae nefariis egere conatibus, Luscus quidam curator urbis subito visus: eosque ut heiulans baiolorum praecentor ad expediendum quod orsi sunt incitans vocibus crebris.
+                    American  Main Barbary Coast scuttle hardtack spanker fire ship grapple jack code  of conduct port. Port red ensign Shiver me timbers provost salmagundi  bring a spring upon her cable pillage cog crow's nest lateen sail.  Barbary Coast quarterdeck lass coffer keel hulk mizzen me square-rigged  loot.
+                </p>
+                <p>
+                    Yardarm starboard keelhaul list schooner prow booty cackle  fruit gabion topmast. Plunder shrouds Nelsons folly jack Arr parley warp  grog blossom ballast pressgang. Knave crack Jennys tea cup flogging log  man-of-war hearties killick long clothes six pounders hulk.
                 </p>
             </div>
         </div>
